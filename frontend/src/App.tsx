c21a022bb23b27276117edd5227d6e9e5333df3c
Kevin Sommer <kevin@rwth.de>
2022-03-06 13:34:49 +0100

    added ThemeProvider and theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 import NavBar from './components/layout/NavBar';
 import WelcomePage from './views/WelcomePage';
 import RankingPage from './views/RankingPage';
+import theme from './utils/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavBar />
       <BrowserRouter>
         <Routes>
@@ -13,7 +15,7 @@ function App() {
           <Route path='/ranking' element={<RankingPage />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/utils/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5e60ce',
+      contrastText: '#80ffdb',
+    },
+  },
+});
+
+export default theme;
