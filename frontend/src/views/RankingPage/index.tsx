02f7a2eaa4475f15153bab44d89f7a6794a8b670
Kevin Sommer <kevin@rwth.de>
2022-03-08 20:45:04 +0100

    added api call to ranking page

--- frontend/src/views/RankingPage/index.tsx
+++ frontend/src/views/RankingPage/index.tsx
@@ -1,20 +1,32 @@
+import { useEffect, useState } from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-
-const rows: GridRowsProp = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
+import axios from 'axios';
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  { field: 'artist_name', headerName: 'Artist Name', width: 150 },
+  { field: 'title', headerName: 'Title', width: 300 },
+  { field: 'elo', headerName: 'ELO', width: 100 },
 ];
 
+const baseURL = 'http://localhost:8000';
+
 const RankingPage = () => {
+  const [rows, setRows] = useState<GridRowsProp>([]);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/items`).then((res) => {
+      setRows(res.data);
+    });
+  }, []);
+
   return (
-    <div style={{ height: 400, width: '40%', margin: '3em auto 0 auto' }}>
-      <DataGrid autoHeight rows={rows} columns={columns} />
+    <div style={{ height: 600, width: '40%', margin: '3em auto 0 auto' }}>
+      <DataGrid
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+        rows={rows}
+        columns={columns}
+      />
     </div>
   );
 };
