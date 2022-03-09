0fcdb5ff872bb0fd3612f1a8d2090bc0fdb46181
Kevin Sommer <kevin@rwth.de>
2022-03-09 15:53:29 +0100

    modified japanese_prints

--- backend/japanese_prints.py
+++ backend/japanese_prints.py
@@ -25,6 +25,15 @@ for idx, objID in enumerate(tqdm(ukiyo_objects)):
     rows.append({ k: res[k] for k in artwork_keys})
 
 ukiyo_df = pd.DataFrame(rows)
+
+ukiyo_df[ukiyo_df['medium'].str.contains('Woodblock') | ukiyo_df['medium'].str.contains('Hanging scroll')]
+ukiyo_df[ukiyo_df['isPublicDomain'] == True]
+ukiyo_df = ukiyo_df.drop(columns=['isPublicDomain'])
+ukiyo_df = ukiyo_df.rename({'objectID': 'object_id', 'primaryImage': 'object_img', 'primaryImageSmall': 'object_img_small', 
+    'objectURL': 'object_url', 'artistAlphaSort': 'artist_name', 'objectBeginDate': 'object_begin_date', 'objectEndDate': 'object_end_date'})
+
+ukiyo_df['elo'] = 1000
+
 ukiyo_df.to_csv('ukiyo.csv', index=False, sep=';')
 print(ukiyo_df)
 print('FINISHED')
