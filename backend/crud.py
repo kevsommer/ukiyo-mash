b57022150fa4076c4cc29cb3455cd94909f80976
Kevin Sommer <kevsommer96@gmail.com>
2022-03-10 19:54:19 +0100

    removed limit from GET /items request

--- backend/crud.py
+++ backend/crud.py
@@ -7,8 +7,8 @@ import models, schemas
 def get_item(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
 
-def get_items(db: Session, skip: int = 0, limit: int = 1000):
-    return db.query(models.Item).order_by(models.Item.elo.desc()).offset(skip).limit(limit).all()
+def get_items(db: Session, skip: int = 0):
+    return db.query(models.Item).order_by(models.Item.elo.desc()).offset(skip).all()
 
 def get_random_items(db: Session):
     return db.query(models.Item).order_by(func.random()).limit(2).all()
