28bb8b37201b1525f7921fd10ddc11b85724b673
Kevin Sommer <kevin@rwth.de>
2022-03-10 17:16:58 +0100

    moved ContainedImage to components/layout

---
+++ frontend/src/components/layout/ContainedImage/index.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const ContainedImage = styled('img')`
+  height: 50vh;
+  width: 100%;
+  object-fit: scale-down;
+  display: flex;
+  margin: 0 auto;
+`;
+
+export default ContainedImage;
