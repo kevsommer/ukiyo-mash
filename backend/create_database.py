2fb02991f670d0b7de6a8022e5d3249501bcdc91
kevsommer <kevin.sommer96@web.de>
2023-03-27 19:49:38 +0200

    updated create_database script

--- backend/create_database.py
+++ backend/create_database.py
@@ -9,7 +9,12 @@ c.execute('''CREATE TABLE ukiyo(id integer NOT NULL, title text NOT NULL, artist
     object_img_small text NOT NULL, medium text NOT NULL)''')
 
 # load the data into a Pandas DataFrame
-items = pd.read_csv('ukiyo.csv', sep=';')
+items_df = pd.read_csv('ukiyo.csv', sep=';')
+items_df['elo'] = 1000
+items_df = items_df.dropna()
+items_df = items_df.rename(columns={'objectID': 'id', 'artistAlphaSort': 'artist_name', 'objectBeginDate': 'object_begin_date', 
+                              'objectEndDate': 'object_end_date', 'objectURL': 'object_url', 'primaryImage': 'object_img',  
+                              'primaryImageSmall': 'object_img_small'})
 
 # write the data to a sqlite table
-items.to_sql('ukiyo', conn, if_exists='append', index = False)
+items_df.to_sql('ukiyo', conn, if_exists='append', index = False)
