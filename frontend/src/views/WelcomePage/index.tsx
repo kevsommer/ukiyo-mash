a8717e317ab3c69b49fe00b84a185e80d863922e
Kevin Sommer <kevsommer96@gmail.com>
2023-03-27 18:03:01 +0200

    added WelcomePage

--- frontend/src/views/WelcomePage/index.tsx
+++ frontend/src/views/WelcomePage/index.tsx
@@ -1,3 +1,40 @@
-const WelcomePage = () => <div>Welcome Page</div>;
+import { Container, Typography } from '@mui/material';
+import ContainedImage from '../../components/layout/ContainedImage';
+import mash from '../../assets/mash.png';
+import ranking from '../../assets/ranking.png';
+
+const WelcomePage = () => (
+  <Container maxWidth='md' sx={{ maxHeight: '90vh', overflow: 'auto' }}>
+    <Typography
+      color='secondary'
+      variant='h4'
+      sx={{ marginTop: '2vh', textAlign: 'center' }}
+    >
+      Explore Ukiyo-e's
+    </Typography>
+    <ContainedImage
+      style={{ height: '35vh', margin: '5vh 0' }}
+      src={
+        'https://collectionapi.metmuseum.org/api/collection/v1/iiif/51086/135477/main-image'
+      }
+    />
+    <Typography sx={{ textAlign: 'center' }} color='secondary' variant='h4'>
+      Compare Them
+    </Typography>
+    <Typography
+      color='secondary'
+      sx={{ my: '1em', fontSize: '18px' }}
+    ></Typography>
+    <ContainedImage style={{ marginTop: '5vh' }} src={mash} alt='mash' />
+    <Typography
+      sx={{ textAlign: 'center', margin: '5vh 0' }}
+      color='secondary'
+      variant='h4'
+    >
+      View Rankings
+    </Typography>
+    <ContainedImage style={{ marginTop: '5vh' }} src={ranking} alt='mash' />
+  </Container>
+);
 
 export default WelcomePage;
