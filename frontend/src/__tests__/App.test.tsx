266952f05e2d5833474ddd6777f0641bb16e3826
kevsommer <kevsommer96@gmail.com>
2024-10-03 22:24:11 +0200

    quick frontend update

--- frontend/src/__tests__/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders correctly', () => {
-  const component = render(<App />);
-  expect(component).toMatchSnapshot();
-});
--- frontend/src/views/WelcomePage/__tests__/test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import WelcomePage from '../index';
-
-test('renders correctly', () => {
-  const component = render(<WelcomePage />);
-  expect(component).toMatchSnapshot();
-});
