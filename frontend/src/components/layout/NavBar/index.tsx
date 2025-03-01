a95a1a40cd9d2fa90805c16bca2bb91c6e565130
kevsommer <kevsommer96@gmail.com>
2025-03-01 14:26:41 +0100

    refactors NavBar

--- frontend/src/components/layout/NavBar/index.tsx
+++ frontend/src/components/layout/NavBar/index.tsx
@@ -1,42 +1,50 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import styled from '@emotion/styled';
 
 const pages = ['ranking', 'mash', 'about'];
 
-const NavBar = () => (
-  <AppBar position='static'>
-    <Toolbar>
-      <IconButton
-        href='/'
-        size='large'
-        edge='start'
-        color='inherit'
-        aria-label='menu'
-      >
-        浮世絵
-        <Typography variant='h5' component='div' sx={{ mx: '1vw' }}>
-          Ukiyo Mash
-        </Typography>
-      </IconButton>
+const AppBar = styled('div')`
+  position: static;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: var(--primary-color);
+  color: var(--contrast-text);
+`;
 
-      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-        {pages.map((page) => (
-          <Button
-            key={page}
-            href={page}
-            onClick={() => {}}
-            sx={{ my: 2, color: 'white', display: 'block' }}
-          >
-            {page}
-          </Button>
-        ))}
-      </Box>
+const Toolbar = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding: 1rem;
+  align-items: center;
+`;
+
+const HomeLink = styled('a')`
+  text-decoration: none;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: row;
+  width: fit-content;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+const PageLink = styled('a')`
+  color: white;
+  text-decoration: none;
+  padding: 0 1rem;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+
+const NavBar = () => (
+  <AppBar>
+    <Toolbar>
+      <HomeLink>
+        浮世絵
+        Ukiyo Mash
+      </HomeLink>
+      {pages.map((page) => (<PageLink href={page}>{page}</PageLink>))}
     </Toolbar>
   </AppBar>
 );
